33fb50abc7a35d321b1ecd8054c089833c4308e9
JimmyK <dacosta.s@northeastern.edu>
2023-03-26 07:17:16 -0400

    test update to fix redir as suggested in piazza @870

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import ExplorePage from "./explore/ExplorePage";
-import HomePage from "./home";
+import HomePage from "./homenew";
 import whoReducer
     from "./reducers/who-reducer";
 import tuitsReducer from "./tuits/tuits-reducer";
